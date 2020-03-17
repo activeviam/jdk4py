ec472cd26c2ca2cb300f5256d720c00de79b0f0b
fabiencelier <fabien.celier@polytechnique.edu>
2020-03-17 12:54:02 +0100

    Package all java runtime files

--- README.md
+++ README.md
@@ -18,49 +18,8 @@ from atotijdk import java
 java("myJar.jar")
 ```
 
-Additional JVM and Popen arguments can be passed.
+Some JVM arguments can be provided, any additional argument will be passed to Popen:
 
 ```python
 java_run("myJar.jar", jvm_args=["-xmx=16G"], stdout=PIPE, stderr=PIPE)
-```
-
-## Build, test and deploy
-
-
-### Install dependencies
-
-```bash
-pipenv install
-```
-
-When building the wheel the Specify the platform of the build must be added according to [naming convention](https://www.python.org/dev/peps/pep-0427/#file-name-convention)
-
-### Build the JDK executable
-
-```bash
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.se,jdk.unsupported --output atoti-jdk/java-runtime
-```
-
-### Test
-
-```bash
-pipenv run pytest
-```
-
-### Build the wheel
-
-```bash
-pipenv run python setup.py bdist_wheel --plat-name manylinux1_x86_64
-```
-
-Other platform names:
-  - win-amd64
-  - macosx_10_11_x86_64
-
-### Deploy
-
-Set the token as envrionment variable
-
-```bash
-pipenv run twine upload dist/* --username __token__ --password $ATOTI_TOKEN
 ```
--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
 """Setup"""
 
+import glob
 import setuptools
 
-from os import path
+from distutils.dir_util import copy_tree
+from os import path, walk
+from pathlib import Path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+file_directory = path.abspath(path.dirname(__file__))
 
 def get_package_version():
     """Read the version of the package.
     See https://packaging.python.org/guides/single-sourcing-package-version
     """
     version_exports = {}
-    file_directory = path.abspath(path.dirname(__file__))
     with open(path.join(file_directory, "atotijdk", "version.py")) as file:
         exec(file.read(), version_exports)  # pylint: disable=exec-used
     return version_exports["VERSION"]
+
+def get_java_files():
+    return [
+        str(Path(f).relative_to("atotijdk"))
+        for f in glob.glob(
+            path.join("atotijdk", "java-runtime", "**"),
+            recursive=True
+        )
+    ]
 
 setuptools.setup(
     name="atoti-jdk",
@@ -27,7 +40,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/atoti/atoti-jdk",
     packages=setuptools.find_packages(exclude=["tests"]),
-    package_data={"atotijdk": ["java-runtime/*/*/*/*/*"]},
+    package_data={"atotijdk": get_java_files()},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
