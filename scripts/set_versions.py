f4cd14b2422f0f2ab3321cfe5118f8000ed6152a
bloussou <brieuc.loussouarn@orange.fr>
2021-01-04 10:40:48 +0100

    write in file test

--- scripts/set_versions.py
+++ scripts/set_versions.py
@@ -8,7 +8,10 @@ _LIB_VERSION_FILENAME = "lib_version.txt"
 
 def set_env_variable_in_github_job(name: str, value: str):
     # See https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable.
-    os.popen(f"echo \"{name}={value}\" >> $GITHUB_ENV")
+    f=open(os.environ["GITHUB_ENV"], "a")
+    f.write(f"{name}={value}")
+    f.close()
+    # os.popen(f"echo \"{name}={value}\" >> $GITHUB_ENV")
 
 
 def set_java_version_env_variable_in_github_job():
