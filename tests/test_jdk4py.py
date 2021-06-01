a7381b2ecf94f7583776cce50922f5b7943e2e97
Thibault Derousseaux <tibdex@gmail.com>
2021-06-01 15:16:54 +0200

    Make some improvements

--- tests/test_jdk4py.py
+++ tests/test_jdk4py.py
@@ -1,32 +1,26 @@
 from pathlib import Path
 from subprocess import PIPE
 
+from jdk4py import JAVA, JAVA_HOME, JAVA_VERSION, MAJOR_JAVA_VERSION, execute_jar, java
+
 _TESTS_DIRECTORY = Path(__file__).parent
 
 
 def test_java_version():
-    from jdk4py import java, JAVA_VERSION
-
     process = java(["--version"], stdout=PIPE, stderr=PIPE)
     out, err = process.communicate()
+    assert not err
     version = str(out).split("\n")[0].split(" ")[1]
     assert version[:version.rindex(".")] == JAVA_VERSION
-    assert err == b""
 
 def test_major_java_version():
-    from jdk4py import MAJOR_JAVA_VERSION
-
     assert MAJOR_JAVA_VERSION == "11"
 
 def test_java_home():
-    from jdk4py import JAVA, JAVA_HOME
-
     assert JAVA == JAVA_HOME / "bin" / "java"
 
 
 def test_hello_world_jar():
-    from jdk4py import execute_jar
-
     # The JAR can be regenerated like that:
     #    rm resources/*.class
     #    rm resources/*.jar
@@ -36,6 +30,6 @@ def test_hello_world_jar():
     path = _TESTS_DIRECTORY / "resources" / "hello.jar"
     process = execute_jar(path.absolute(), stdout=PIPE, stderr=PIPE)
     out, err = process.communicate()
-    assert out == b"Hello, World\n" or out == b"Hello, World\r\n"
-    assert err == b""
+    assert not err
     assert process.returncode == 0
+    assert out == b"Hello, World\n" or out == b"Hello, World\r\n"
