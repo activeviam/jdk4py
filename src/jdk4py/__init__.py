27ed56435c0f1048e578576163c4af67c48e0d8b
Thibault Derousseaux <tibdex@gmail.com>
2024-08-29 18:41:44 -0400

    fixup! Add support for Linux and Windows Arm64

--- src/jdk4py/__init__.py
+++ src/jdk4py/__init__.py
@@ -8,8 +8,7 @@ _PACKAGE_DIRECTORY = Path(__file__).parent
 JAVA_HOME = _PACKAGE_DIRECTORY / "java-runtime"
 JAVA = JAVA_HOME / "bin" / "java"
 
-
-_VERSION: str = json.loads((_PACKAGE_DIRECTORY / "version.json").read_bytes())
-JAVA_VERSION: tuple[int, int, int] = tuple(
-    int(number) for number in _VERSION.split(".")[:3]
-)
+_VERSION = json.loads((_PACKAGE_DIRECTORY / "version.json").read_bytes())
+assert isinstance(_VERSION, str)
+_MAJOR, _MINOR, _PATCH = tuple(int(number) for number in _VERSION.split(".")[:3])
+JAVA_VERSION = _MAJOR, _MINOR, _PATCH
