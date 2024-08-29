af1fb02c8e19a8c041f3d5c86a02fb62342092e5
Thibault Derousseaux <tibdex@gmail.com>
2024-08-29 17:20:02 -0400

    fixup! Add support for Linux and Windows Arm64

--- scripts/print_java_version.py
+++ scripts/print_java_version.py
@@ -2,5 +2,5 @@ from jdk4py import JAVA_VERSION
 
 if __name__ == "__main__":
     print(  # noqa: T201
-        f'java-version="{".".join(str(number) for number in JAVA_VERSION)}"',
+        f'java-version={".".join(str(number) for number in JAVA_VERSION)}',
     )
