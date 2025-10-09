0e2865d9a702b5a365042fd0e20b06dbb8fadd03
Thibault Derousseaux <6574550+tibdex@users.noreply.github.com>
2025-10-09 11:03:32 -0400

    Try uv_build backend

--- src/jdk4py/__init__.py
+++ src/jdk4py/__init__.py
@@ -1,6 +1,6 @@
 """JDK packaged for Python."""
 
-import json
+from importlib.metadata import version as _version
 from pathlib import Path
 
 _PACKAGE_DIRECTORY = Path(__file__).parent
@@ -8,7 +8,6 @@ _PACKAGE_DIRECTORY = Path(__file__).parent
 JAVA_HOME = _PACKAGE_DIRECTORY / "java-runtime"
 JAVA = JAVA_HOME / "bin" / "java"
 
-_VERSION = json.loads((_PACKAGE_DIRECTORY / "version.json").read_bytes())
-assert isinstance(_VERSION, str)
+_VERSION = _version("jdk4py")
 _MAJOR, _MINOR, _PATCH = tuple(int(number) for number in _VERSION.split(".")[:3])
 JAVA_VERSION = _MAJOR, _MINOR, _PATCH
