21f148c2298c6a54955de73826d619b28de73acb
Thibault Derousseaux <6574550+tibdex@users.noreply.github.com>
2025-10-13 16:58:39 -0400

    Modernize

--- src/jdk4py/__init__.py
+++ src/jdk4py/__init__.py
@@ -1,14 +1,15 @@
 """JDK packaged for Python."""
 
-import json
-from pathlib import Path
+from importlib.metadata import version as _version
+from pathlib import Path as _Path
 
-_PACKAGE_DIRECTORY = Path(__file__).parent
+_PACKAGE_DIRECTORY = _Path(__file__).parent
 
 JAVA_HOME = _PACKAGE_DIRECTORY / "java-runtime"
 JAVA = JAVA_HOME / "bin" / "java"
 
-_VERSION = json.loads((_PACKAGE_DIRECTORY / "version.json").read_bytes())
-assert isinstance(_VERSION, str)
-_MAJOR, _MINOR, _PATCH = tuple(int(number) for number in _VERSION.split(".")[:3])
-JAVA_VERSION = _MAJOR, _MINOR, _PATCH
+_VERSION = _version("jdk4py")
+_JAVA_MAJOR, _JAVA_MINOR, _JAVA_PATCH, _LIB_VERSION = [
+    int(number) for number in _VERSION.split(".")
+]
+JAVA_VERSION = _JAVA_MAJOR, _JAVA_MINOR, _JAVA_PATCH
